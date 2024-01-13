a6bc9248262d276ab83c951bbdfbae14f04b1324
wupj <wdotl@outlook.com>
2024-01-13 17:14:57 +0800

    feat: 增加notfound页面

--- src/views/not-found/index.vue
+++ src/views/not-found/index.vue
@@ -1,13 +1,24 @@
 <template>
-
+  <div class="container">
+    <div class="h-full flex flex-column justify-content-center align-items-center">
+      <SvgIcon iconName="notfound" className="notfound" />
+      <Button label="返回首页" @click="handleReturn" />
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
-  name: "notFound"
-}
+<script lang="ts" setup>
+  import { useRouter } from 'vue-router'
+
+  const router = useRouter()
+  const handleReturn = () => {
+    router.push('/home')
+  }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  .notfound {
+    width: 50rem;
+    height: 50rem;
+  }
 </style>
